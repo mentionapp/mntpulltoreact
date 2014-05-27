b4146d694211c5920975304cfc6c633e89166150
gsempe <guillaume.sempe@gmail.com>
2014-05-27 22:33:28 +0200

    Example : Add a button to launch a reaction without gesture

--- Example/Example/RootTableViewDelegateAndDataSource.m
+++ Example/Example/RootTableViewDelegateAndDataSource.m
@@ -22,7 +22,7 @@
     if (nil == self) {
         return nil;
     }
-    _data = @[@"redColor", @"greenColor", @"whiteColor"];
+    _data = @[@"redColor", @"greenColor", @"whiteColor", @"Reaction 1"];
     return self;
 }
 
@@ -32,19 +32,33 @@
 {
     // Change react control according to the cell selected
     NSInteger index = [indexPath row] % [self.data count];
-    MNTPullToReactControl *reactControl = [[MNTPullToReactControl alloc] initWithNumberOfActions:4];
     if ([[_data objectAtIndex:index] isEqualToString:@"redColor"]) {
+        MNTPullToReactControl *reactControl = [[MNTPullToReactControl alloc] initWithNumberOfActions:4];
         reactControl.backgroundColor = [UIColor redColor];
+        [reactControl addTarget:self action:@selector(reaction:) forControlEvents:UIControlEventValueChanged];
+        tableView.reactControl = reactControl;
     }
     if ([[_data objectAtIndex:index] isEqualToString:@"greenColor"]) {
+        MNTPullToReactControl *reactControl = [[MNTPullToReactControl alloc] initWithNumberOfActions:4];
         reactControl.backgroundColor = [UIColor greenColor];
+        [reactControl addTarget:self action:@selector(reaction:) forControlEvents:UIControlEventValueChanged];
+        tableView.reactControl = reactControl;
     }
     if ([[_data objectAtIndex:index] isEqualToString:@"whiteColor"]) {
+        MNTPullToReactControl *reactControl = [[MNTPullToReactControl alloc] initWithNumberOfActions:4];
         reactControl.backgroundColor = [UIColor whiteColor];
+        [reactControl addTarget:self action:@selector(reaction:) forControlEvents:UIControlEventValueChanged];
+        tableView.reactControl = reactControl;
     }
-    [reactControl addTarget:self action:@selector(reaction:) forControlEvents:UIControlEventValueChanged];
-
-    tableView.reactControl = reactControl;
+    if ([[_data objectAtIndex:index] isEqualToString:@"Reaction 1"]) {
+        [tableView.reactControl beginAction:1];
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+            usleep(1100 * 1000);
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [tableView.reactControl endAction:tableView.reactControl.action];
+            });
+        });
+    }
 }
 
 #pragma mark - Data Source
